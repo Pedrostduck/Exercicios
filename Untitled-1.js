37e134238811ce8d579eadf853f88a190a69810e
Pedrostduck <160145564+Pedrostduck@users.noreply.github.com>
2024-10-09 08:16:10 -0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,58 @@
+
+
+let nome = "Alice";
+console.log ("Nome: " + nome);
+
+
+
+console.log ("Idade: " + idade);
+
+
+
+let inscrito = true;
+console.log ("Está inscrito? " + inscrito);
+
+
+
+let pessoa = {
+nome: "Alice",
+idade: 25,
+inscrito: true,
+curso: "Introdução à Programação"
+};
+
+console.log("Informações sobre a pessoa:");
+console.log(pessoa);
+
+
+if (pessoa.inscrito) {
+
+console.log(pessoa.nome + " está inscrita no curso de "+ pessoa.curso + ".");
+} else {
+console.log(pessoa.nome +  "não está inscrita em nenhum curso.");
+
+}
+
+let nome = "Alice";
+console.log("Nome: " + nome);
+
+console.log("Idade: "+ idade);
+
+let inscrito = true;
+console.log("Está inscrito? " + inscrito);
+
+let pessoa = {
+nome: "Alice",
+idade: 25,
+inscrito: true,
+curso: "Introdução à Programação"
+};
+console.log("Informações sobre a pessoa:");
+console.log(pessoa);
+
+if (pessoa.inscrito) 
+    {
+    console.log (pessoa.nome  + " está inscrita no curso de " + pessoa.curso + ".");
+    } else {
+    console.log (pessoa.nome  + " não está inscrita em nenhum curso.");
+    }
